d90226f7a061c5d1f06acd721787ce50a3df8ef0
Mirek Zvolský <zvolsky@seznam.cz>
2021-02-17 18:11:42 +0100

    end of testing; original state

--- advanced_filters/forms.py
+++ advanced_filters/forms.py
@@ -269,7 +269,7 @@ class AdvancedFilterForm(CleanWhiteSpacesMixin, forms.ModelForm):
                         "AdvancedFilterForm: skip invalid field - %s", e
                     )
                     continue
-            model_fields[field] = verbose_name + 'xxx'
+            model_fields[field] = verbose_name
         return model_fields
 
     def __init__(self, *args, **kwargs):
